2c2c8bd61693d543b2d3ab9be96aa8ee5f4c09fb
Joren <63969493+joren961@users.noreply.github.com>
2021-03-30 21:13:59 +0200

    Update DetailsController.js

--- CROWDR_Joren_Fedor/Controllers/DetailsController.js
+++ CROWDR_Joren_Fedor/Controllers/DetailsController.js
@@ -20,19 +20,8 @@ class DetailsController {
         label.innerText = type + " " + object._id;
         details.appendChild(label);
 
-        let newLabel = document.createElement('label');
-        let newInput = document.createElement('input');
-        newInput.required = true;
-        newLabel.innerText = "Maximum visitors:";
-        newInput.setAttribute('type', 'number');
-        newInput.setAttribute('min', '0');
-        newInput.setAttribute('placeholder', '0');
-        newInput.className= "StandardNumberInput";
+        this.addInputBasedOnType(type,details);
 
-        this.addExtraInputBasedOnType(type,details,newLabel);
-
-        details.appendChild(newLabel);
-        details.appendChild(newInput);
         let submit = document.createElement('a');
         submit.addEventListener('click',()=>this.submitDetails(object, type));
         submit.className="button";
@@ -42,8 +31,21 @@ class DetailsController {
         gridView.appendChild(details);
     }
 
-    addExtraInputBasedOnType(type,details,newLabel) {
+    addInputBasedOnType(type,details) {
+        let newLabel = document.createElement('label');
+        let newInput = document.createElement('input');
+        newInput.required = true;
+        newLabel.innerText = "Maximum visitors:";
+        newInput.setAttribute('type', 'number');
+        newInput.setAttribute('min', '0');
+        newInput.setAttribute('placeholder', '0');
+        newInput.className= "MaxVisitors";
+
         switch (type) {
+            case "Drink stand":
+                details.appendChild(newLabel);
+                details.appendChild(newInput);
+                break;
             case "Food stand":
                 let foodLabel = document.createElement('label');
                 let foodInput = document.createElement('input');
@@ -51,7 +53,9 @@ class DetailsController {
                 foodInput.setAttribute("type","text");
                 foodInput.style.width = "150px";
                 foodInput.setAttribute('placeholder','For example: Kebab');
-                foodInput.className= "FoodType";
+                foodInput.id= "FoodType";
+                details.appendChild(newLabel);
+                details.appendChild(newInput);
                 details.appendChild(foodLabel);
                 details.appendChild(foodInput);
                 break;
@@ -63,17 +67,22 @@ class DetailsController {
                 tentInput.style.width = "100px";
                 tentInput.setAttribute('placeholder','00:00');
                 tentInput.id ="TentOpeningTime";
+                details.appendChild(newLabel);
+                details.appendChild(newInput);
                 details.appendChild(tentLabel);
                 details.appendChild(tentInput);
                 break;
+            case "Toilet building":
+                break;
             case "Trashcan":
                 newLabel.innerText = "Capacity in KG";
+                newInput.className = "TrashCapacity";
                 let trashLabel = document.createElement('label');
                 trashLabel.innerText = "Emptying time in hours";
                 let trashInput = document.createElement('input');
                 trashInput.setAttribute('type','number');
                 trashInput.setAttribute('placeholder','0');
-                trashInput.id="TrashCapacity";
+                trashInput.id="TrashEmptyTime";
                 details.appendChild(trashLabel);
                 details.appendChild(trashInput);
                 break;
@@ -99,29 +108,34 @@ class DetailsController {
     }
 
     submitDetails(object, type) {
-        let maxVisitors = document.querySelector('.StandardNumberInput').value;
-        object._maxVisitors = maxVisitors;
+        let maxVisitors = parseInt(document.querySelector('.MaxVisitors').value);
+        let foodType = document.querySelector('#FoodType').value;
+        let tentTime = document.querySelector('#TentOpeningTime').value;
+        let trashCapacity = parseInt(document.querySelector('.TrashCapacity').value);
+        let trashTime = parseInt(document.querySelector('.TrashEmptyTime').value);
         switch (type) {
             case "Food stand":
-
+                object._maxVisitors = maxVisitors;
+                object._foodType = foodType;
                 break;
             case "Drink stand":
-
+                object._maxVisitors = maxVisitors;
                 break;
             case "Tent":
-
+                object._maxVisitors = maxVisitors;
+                object._openingTime = tentTime;
                 break;
             case "Toilet building":
-
                 break;
             case "Trashcan":
-
+                object._capacity = trashCapacity;
+                object._emptyingTime = trashTime;
                 break;
             case "Tree":
-                //set tree object type, sla op in localstorage
+                
                 break;
         }
-
+        console.log(object);
         this._storageController.updateRegionObject(this._regionName,object);
     }
 }
