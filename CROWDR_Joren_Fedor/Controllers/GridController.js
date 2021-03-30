ff3bb6c2ca8a94160e15bfe0af4cc1c80b5aee17
Fedor <Fedor@DESKTOP-C8VMLEN>
2021-03-30 20:22:38 +0200

    updated agloritm, now there is an object for each item

--- CROWDR_Joren_Fedor/Controllers/GridController.js
+++ CROWDR_Joren_Fedor/Controllers/GridController.js
@@ -69,35 +69,28 @@ class GridController {
     renderMenu(region)
     {
         this._optionView.innerHTML = '';
-        for (const foodstand of region._foodstands) {
-            if (foodstand!=null) {
-                this.renderMenuItem(foodstand,"Food stand","Resources/foodStand(1x1).png",region._foodstands.length);
-            }
-        }
-        for (const drinkstand of region._drinkstands) {
-            if (drinkstand!=null) {
-                this.renderMenuItem(drinkstand,"Drink stand","Resources/drinkStand(1x2).png",region._drinkstands.length);
-            }
-        }
-        for (const tent of region._tents) {
-            if (tent!=null) {
-                this.renderMenuItem(tent,"Tent","Resources/tent(3x3).png",region._tents.length);
-            }
-        }
-        for (const toiletbuilding of region._toiletbuildings) {
-            if (toiletbuilding!=null) {
-                this.renderMenuItem(toiletbuilding,"Toilet building","Resources/toiletbuilding(1x3).jpg", region._toiletbuildings.length);
-            }
-        }
-        for (const trashcan of region._trashcans) {
-            if (trashcan!=null) {
-                this.renderMenuItem(trashcan,"Trashcan","Resources/trashcan(1x1).jpg",region._trashcans.length);
-            }
-        }
+
+        //FOODSTAND
+        this.renderMenuItem(region._foodstands,"Food stand","Resources/foodStand(1x1).png",region._foodstands.length);
+
+        //DRINKSTAND
+        this.renderMenuItem(region._drinkstands,"Drink stand","Resources/drinkStand(1x2).png",region._drinkstands.length);
+
+        //TENTS
+        this.renderMenuItem(region._tents,"Toilet building","Resources/tent(3x3).png", region._tents.length);
+
+        //TOILETBUILDING
+        this.renderMenuItem(region._toiletbuildings,"Toilet building","Resources/toiletbuilding(1x3).jpg", region._toiletbuildings.length);
+
+        //TRASHCAN
+        this.renderMenuItem(region._trashcans,"Trashcan","Resources/trashcan(1x1).jpg",region._trashcans.length);
+
+        //TREE
+
     }
 
     //moet ervoor zorgen dat elk object apart gemaakt wordt en op de stapel objecten van die soort wordt gegooid
-    renderMenuItem(object,type, imagesrc, amount)
+    renderMenuItem(objectArray,type, imagesrc, amount)
     {
         let newMenuItem = document.createElement('div');
         let newTitle = document.createElement('p');
@@ -106,38 +99,48 @@ class GridController {
         newMenuItem.appendChild(newTitle);
         newMenuItem.className = 'menuItemWrapper';
         let newSquare = document.createElement('div');
-        let newDragble = document.createElement('img');
 
-        newDragble.src = imagesrc;
-        newDragble.id = type;
-        //newDragble.addEventListener('dragstart', () => this.drag(event));
-        newDragble.addEventListener("dragstart", e => {
-           e.dataTransfer.setData("text/plain", newDragble.id);
-        });
-        newDragble.draggable = true;
-        newDragble.className = 'newDragble';
-        newDragble.addEventListener('click',(e) => this._DetailsController.openDetails(object,type,this._gridview));
-        newSquare.appendChild(newDragble);
+        for (const object of objectArray) {
+            if (object!=null) {
+                console.log(object);
+                this.createnewDragble(newSquare, type, 5, imagesrc);
+            }
+        }
         newMenuItem.appendChild(newSquare);
         this._optionView.appendChild(newMenuItem);
     }
 
-
-
-    allowDrop(ev) {
-        ev.preventDefault();
-        console.log("adasd");
-        alert('test');
+    createnewDragble(parentObject , type, id, imagesrc)
+    {
+        let newDragble = document.createElement('img');
+        newDragble.src = imagesrc;
+        newDragble.id = type;
+        //newDragble.addEventListener('dragstart', () => this.drag(event));
+        newDragble.addEventListener("dragstart", e => {
+            e.dataTransfer.setData("text/plain", newDragble.id);
+        });
+        newDragble.draggable = true;
+        newDragble.className = id;
+        newDragble.addEventListener('click',(e) => this._DetailsController.openDetails(object,type,this._gridview));
+        parentObject.appendChild(newDragble);
     }
 
-    drag(ev) {
-        ev.dataTransfer.setData("text", ev.target.id);
-        //alert('test');
-    }
+    // allowDrop(ev) {
+    //     ev.preventDefault();
+    //     console.log("adasd");
+    //     alert('test');
+    // }
+    //
+    // drag(ev) {
+    //     ev.dataTransfer.setData("text", ev.target.id);
+    // }
+    //
+    // drop(ev) {
+    //     ev.preventDefault();
+    //     let data = ev.dataTransfer.getData("text");
+    //     ev.target.appendChild(document.getElementById(data));
+    // }
 
-    drop(ev) {
-        ev.preventDefault();
-        let data = ev.dataTransfer.getData("text");
-        ev.target.appendChild(document.getElementById(data));
-    }
+
+
 }
