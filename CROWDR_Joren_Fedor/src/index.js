d89e67e590e493b60fad6d88febecf72ce97c643
Fedor <Fedor@DESKTOP-C8VMLEN>
2021-04-02 16:27:22 +0200

    inbetween

--- CROWDR_Joren_Fedor/src/index.js
+++ CROWDR_Joren_Fedor/src/index.js
@@ -9,3 +9,5 @@ let storageController = new StorageController();
 let gridController = new GridController(storageController);
 let navigationController = new NavigationController(storageController, gridController);
 let formController = new FormController(storageController, gridController);
+
+alert("dsasd");
