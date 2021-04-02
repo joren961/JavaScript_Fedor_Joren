5fcd3946871bf715eb1cf4a07d7334e01c97b853
Joren <63969493+joren961@users.noreply.github.com>
2021-04-02 21:58:54 +0200

    small test change

--- CROWDR_Joren_Fedor/cypress/integration/TestFormFail.js
+++ CROWDR_Joren_Fedor/cypress/integration/TestFormFail.js
@@ -1,6 +1,6 @@
 describe('test form failed', () => {
     it('test input', () => {
-        cy.visit('http://localhost:63342/JavaScript_Fedor_Joren/CROWDR_Joren_Fedor/HomePage.html?_ijt=g81uebunvi75838lk78mi62416');
+        cy.visit('http://localhost:63342/JavaScript_Fedor_Joren/CROWDR_Joren_Fedor/HomePage.html?_ijt=i73sqvf6luoo7435k87avqfeeq');
 
         cy.get('.regioninput').should('be.visible');
         cy.get('.regionVisitorInput').should('be.visible').type('30');
@@ -11,14 +11,12 @@ describe('test form failed', () => {
         cy.get('.nextInput').should('be.visible').click();
         cy.get('.drinkStands').should('be.visible').type('1');
         cy.get('.nextInput').should('be.visible').click();
-        cy.get('.trees').should('be.visible').type('-1');
+        cy.get('.trees').should('be.visible').type('1');
         cy.get('.nextInput').should('be.visible').click();
         cy.get('.toilets').should('be.visible').type('1');
         cy.get('.nextInput').should('be.visible').click();
         cy.get('.trash').should('be.visible').type('1');
-        cy.get('.squaresLeft').should('have.text','209');
         cy.get('#submit').should('be.visible').click();
-        cy.get('#validationMessageMin').should('be.visible');
         cy.get('#validationMessageMax').should('be.visible');
         cy.get('#validationMessageFilled').should('be.visible');
         cy.get('#validationMessageSpace').should('be.visible');
