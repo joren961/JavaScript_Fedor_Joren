454b979d018d8e53550b2e41da5cb6ea8deda122
Joren <63969493+joren961@users.noreply.github.com>
2021-04-03 17:46:35 +0200

    added test plugin

--- CROWDR_Joren_Fedor/cypress/integration/TestDragAndDrop.js
+++ CROWDR_Joren_Fedor/cypress/integration/TestDragAndDrop.js
@@ -2,5 +2,6 @@ describe('test form failed', () => {
     it('test input', () => {
         cy.visit('http://localhost:63342/JavaScript_Fedor_Joren/CROWDR_Joren_Fedor/HomePage.html?_ijt=i73sqvf6luoo7435k87avqfeeq');
 
+
     })
 })
